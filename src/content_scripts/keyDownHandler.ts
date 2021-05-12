954958928152b51bb75751f035b98fb836ce0306
anko9801 <daiusa3@icloud.com>
2021-05-12 18:00:46 +0900

    関数にしてまとめたり色々した><

--- src/content_scripts/keyDownHandler.ts
+++ src/content_scripts/keyDownHandler.ts
@@ -18,6 +18,7 @@ const isNotSelectedInput = () => {
 };
 
 let keyState: Map<any, boolean> = new Map();
+let timer: NodeJS.Timeout;
 const nowState = {
   place: 2,
   focused: null,
@@ -25,12 +26,14 @@ const nowState = {
 window.addEventListener('keyup', async (ev) => {
   const { key } = ev;
   keyState.set(key, false);
+  if (ev.altKey || ev.shiftKey || ev.ctrlKey || ev.metaKey) return;
+  clearInterval(timer);
 });
 
 export const handler: (ev: KeyboardEvent) => void = async (ev) => {
   const { key } = ev;
   keyState.set(key, true);
-  if (ev.altKey || ev.shiftKey || ev.ctrlKey || ev.metaKey) return;
+  if (ev.altKey || ev.ctrlKey || ev.metaKey) return; // Shift + Tab使います
   if (isNotSelectedInput()) {
     switch (key) {
       case '1':
@@ -61,7 +64,7 @@ export const handler: (ev: KeyboardEvent) => void = async (ev) => {
         break;
       }
       case 'q':
-        getElements.navigations()[0].click();
+        doFunctions.clickHomeNavigation();
         break;
       case 'w':
         getElements.channels()[0].click();
@@ -145,72 +148,70 @@ export const handler: (ev: KeyboardEvent) => void = async (ev) => {
         getElements.sidebarContent()[0].click();
         break;
       case 'f':
-        nowState.place = (nowState.place - 1 + 3) % 3;
-        console.log(nowState.place);
+        // doFunctions.changeMainStatePrev();
         break;
       case 'g':
-        nowState.place = (nowState.place + 1) % 3;
-        console.log(nowState.place);
+        // doFunctions.changeMainStateNext();
         break;
+      case 'Tab': {
+        ev.preventDefault();
+        if (ev.shiftKey) doFunctions.moveupChannel(true);
+        else doFunctions.movedownChannel(true);
+        break;
+      }
+      case 'v': {
+        doFunctions.openSubchannel();
+        break;
+      }
       case 'j': {
         if (nowState.place == 0) {
-          const navigation_place = getElements.getNavigationIndex();
-          getElements.navigations()[(navigation_place + 1) % 5].click();
+          doFunctions.clickNextNavigation();
           break;
         } else if (nowState.place == 1) {
           const navigation_place = getElements.getNavigationIndex();
           const desktop = getElements.desktopNavigation();
           if (!desktop) break;
 
+          // ホームとチャンネル
           if (navigation_place == 0 || navigation_place == 1) {
-            const channels: NodeListOf<HTMLDivElement> = getAllElementByClassName(
-              'ChannelElement_container'
-            );
-
-            let flag = false;
-            for (let i = 0; i < channels.length; i++) {
-              console.log(i, channels[i]);
-              if (channels[i].getAttribute('aria-selected') == 'true') {
-                if (!channels[i + 1]) break;
-                console.log(channels[i + 1]);
-                flag = true;
-                getAllElementByClassName<HTMLDivElement>(
-                  'ChannelElementName_container'
-                )[i + 1].click();
-                nowState.place = 2;
-              }
-            }
-            if (flag) break;
-
-            getAllElementByClassName<HTMLDivElement>(
-              'ChannelElementName_container'
-            )[0].click();
-            nowState.place = 2;
+            // doFunctions.movedownChannel();
             break;
           } else if (navigation_place == 2) {
             const messagePanels = getAllElementByClassName(
               'MessagePanel_container'
             );
+            break;
           } else {
           }
-          break;
+
+          // メッセージ
         } else if (nowState.place == 2) {
           const viewContainer = getElements.messagesScroller();
           if (!viewContainer) break;
-          const messages = getElements.messages();
-          for (let i = 0; i < messages.length; i++) {
-            if (messages[i].getBoundingClientRect().top > 11.625) {
-              if (!messages[i + 1]) break;
-              viewContainer.scrollTo({
-                top:
-                  viewContainer.scrollTop +
-                  messages[i + 1].getBoundingClientRect().top -
-                  78,
-                behavior: 'smooth',
-              });
-              break;
-            }
-          }
+
+          // 連続スクロール
+          timer = setInterval(() => {
+            viewContainer.scrollTo({
+              top: viewContainer.scrollTop - 78 + 30,
+              behavior: 'smooth',
+            });
+          }, 100);
+
+          // メッセージ毎スクロール
+          // const messages = getElements.messages();
+          // for (let i = 0; i < messages.length; i++) {
+          //   if (messages[i].getBoundingClientRect().top > 11.625) {
+          //     if (!messages[i + 1]) break;
+          //     viewContainer.scrollTo({
+          //       top:
+          //         viewContainer.scrollTop +
+          //         messages[i + 1].getBoundingClientRect().top -
+          //         78,
+          //       behavior: 'smooth',
+          //     });
+          //     break;
+          //   }
+          // }
         }
         break;
       }
@@ -224,29 +225,7 @@ export const handler: (ev: KeyboardEvent) => void = async (ev) => {
           const desktop = getElements.desktopNavigation();
           if (!desktop) break;
           if (navigation_place == 0 || navigation_place == 1) {
-            const channels: NodeListOf<HTMLDivElement> = getAllElementByClassName(
-              'ChannelElement_container'
-            );
-
-            let flag = false;
-            for (let i = 0; i < channels.length; i++) {
-              if (channels[i].getAttribute('aria-selected') == 'true') {
-                if (!channels[i - 1]) break;
-                console.log(channels[i - 1]);
-                flag = true;
-                getAllElementByClassName<HTMLDivElement>(
-                  'ChannelElementName_container'
-                )[i - 1].click();
-                nowState.place = 2;
-                break;
-              }
-            }
-            if (flag) break;
-
-            getAllElementByClassName<HTMLDivElement>(
-              'ChannelElementName_container'
-            )[0].click();
-            nowState.place = 2;
+            // doFunctions.moveupChannel();
             break;
           } else if (navigation_place == 2) {
             const messagePanels = getAllElementByClassName(
@@ -256,26 +235,7 @@ export const handler: (ev: KeyboardEvent) => void = async (ev) => {
           }
           break;
         } else if (nowState.place == 2) {
-          const viewContainer = getElements.messagesScroller();
-          if (!viewContainer) break;
-
-          const messages = getElements.messages();
-          for (let i = messages.length - 1; i >= 0; i--) {
-            if (messages[i].getBoundingClientRect().top < 0) {
-              if (i != 0 && i != 1)
-                messages[i].scrollIntoView({
-                  behavior: 'smooth',
-                  block: 'start',
-                  inline: 'nearest',
-                });
-              else
-                viewContainer.scrollTo({
-                  top: 0,
-                  behavior: 'smooth',
-                });
-              break;
-            }
-          }
+          doFunctions.scrollupMessageByMessage();
         }
         break;
       }
@@ -320,4 +280,212 @@ export const handler: (ev: KeyboardEvent) => void = async (ev) => {
       }
     }
   }
+};
+
+export const doFunctions = {
+  clickNavigationByIndex: (index: number) => {
+    if (0 <= index && index < 5) getElements.navigations()[index].click();
+    else return undefined;
+  },
+  clickNextNavigation: () =>
+    getElements
+      .navigations()
+      [(getElements.getNavigationIndex() + 1) % 5].click(),
+  clickPrevNavigation: () =>
+    getElements
+      .navigations()
+      [(getElements.getNavigationIndex() - 1 + 5) % 5].click(),
+
+  clickHomeNavigation: () => getElements.navigations()[0].click(),
+
+  clickChannelNavigation: () => getElements.navigations()[1].click(),
+  clickChannelMenuByIndex: (index: number) =>
+    getElements.channels()[index].click(),
+  focusChannelMenuSearch: () => getElements.filterInputs()[0].focus(),
+  clickChannelMenuFilterStar: () => getElements.channelFilterStar().click(),
+  movedownChannel: (isLoop: boolean) => {
+    const channels: NodeListOf<HTMLDivElement> = getAllElementByClassName(
+      'ChannelElement_container'
+    );
+    const channels_clicker: NodeListOf<HTMLDivElement> = getAllElementByClassName(
+      'ChannelElementName_container'
+    );
+
+    const index = [...channels].findIndex(
+      (v) => v.getAttribute('aria-selected') == 'true'
+    );
+    const target = channels_clicker[index + 1];
+    if (isLoop) {
+      if (!target) return channels_clicker[0].click();
+      return target.click();
+    }
+    target?.click();
+  },
+  moveupChannel: (isLoop: boolean) => {
+    const channels: NodeListOf<HTMLDivElement> = getAllElementByClassName(
+      'ChannelElement_container'
+    );
+    const channels_clicker: NodeListOf<HTMLDivElement> = getAllElementByClassName(
+      'ChannelElementName_container'
+    );
+
+    const index = [...channels].findIndex(
+      (v) => v.getAttribute('aria-selected') == 'true'
+    );
+    const target = channels_clicker[index - 1];
+    if (isLoop) {
+      if (!target) return channels_clicker[0].click();
+      return target.click();
+    }
+    target?.click();
+  },
+  openSubchannel: () => {
+    const channels: NodeListOf<HTMLDivElement> = getAllElementByClassName(
+      'ChannelElement_container'
+    );
+    const channels_clicker: NodeListOf<HTMLDivElement> = getAllElementByClassName(
+      'ChannelElementHash_container'
+    );
+
+    const index = [...channels].findIndex(
+      (v) => v.getAttribute('aria-selected') == 'true'
+    );
+    channels_clicker[index].click();
+  },
+
+  clickActivityNavigation: () => getElements.navigations()[2].click(),
+  clickActivityFilterNotification: () =>
+    getElements.activityToggleButtons()[0].click(),
+  clickActivityFilterChannel: () =>
+    getElements.activityToggleButtons()[1].click(),
+
+  clickUserNavigation: () => getElements.navigations()[3].click(),
+
+  clickClipNavigation: () => getElements.navigations()[4].click(),
+
+  focusMessageInput: () => getElements.messageInput().focus(),
+  clickMessageInputInsertStampButton: () =>
+    getElements.messageInputInsertStampButton().click(),
+  leaveinput: () => {
+    document.querySelector('body')?.click();
+    getElements.messages().forEach((el) => {
+      el.dispatchEvent(new Event('mouseleave'));
+    });
+  },
+  leaveNo: () => {
+    (
+      (document.activeElement as HTMLInputElement) || HTMLTextAreaElement
+    )?.blur();
+  },
+
+  initState: (place: number) => {
+    nowState.place = place;
+  },
+  changeMainStateNext: () => {
+    if (nowState.place > 0) nowState.place = nowState.place - 1;
+  },
+  changeMainStatePrev: () => {
+    if (nowState.place < 3) nowState.place = nowState.place + 1;
+  },
+
+  scrollLatestMessage: (option: number) => {
+    const messagesScroller = getElements.messagesScroller();
+    if (!messagesScroller) return;
+    messagesScroller.scrollTop = messagesScroller.scrollHeight;
+  },
+  scrollupMessage: () => {
+    const viewContainer = getElements.messagesScroller();
+    if (!viewContainer) return;
+
+    timer = setInterval(() => {
+      viewContainer.scrollTo({
+        top: viewContainer.scrollTop - 78 - 30,
+        behavior: 'smooth',
+      });
+    }, 100);
+  },
+  scrolldownMessage: () => {
+    const viewContainer = getElements.messagesScroller();
+    if (!viewContainer) return;
+
+    timer = setInterval(() => {
+      viewContainer.scrollTo({
+        top: viewContainer.scrollTop - 78 + 30,
+        behavior: 'smooth',
+      });
+    }, 100);
+  },
+  scrollupMessageByMessage: () => {
+    const viewContainer = getElements.messagesScroller();
+    if (!viewContainer) return;
+
+    const messages = getElements.messages();
+    for (let i = messages.length - 1; i >= 0; i--) {
+      if (messages[i].getBoundingClientRect().top < 0) {
+        if (i != 0 && i != 1)
+          messages[i].scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'nearest',
+          });
+        else
+          viewContainer.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          });
+        break;
+      }
+    }
+  },
+  scrolldownMessageByMessage: () => {
+    const viewContainer = getElements.messagesScroller();
+    if (!viewContainer) return;
+
+    // メッセージ毎スクロール
+    const messages = getElements.messages();
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i].getBoundingClientRect().top > 11.625) {
+        if (!messages[i + 1]) break;
+        viewContainer.scrollTo({
+          top:
+            viewContainer.scrollTop +
+            messages[i + 1].getBoundingClientRect().top -
+            78,
+          behavior: 'smooth',
+        });
+        break;
+      }
+    }
+  },
+  openStampsByMessage: (index: number) => {
+    showMessageTool(index);
+    clickMessageTool(0);
+  },
+
+  arrowup: () => {
+    const messageInput = getElements.messageInput();
+    if (messageInput?.value === '' && messageInput === document.activeElement) {
+      showMessageTool(0);
+      clickMessageTool(1);
+      window.setTimeout(() => {
+        if (getElements.messageToolsMenu()[2].innerText !== '編集') {
+          window.setTimeout(() => {
+            document.querySelector('body')?.click();
+          }, 0);
+          return;
+        }
+        getElements.messageToolsMenu()[2].click();
+        document.querySelector('body')?.click();
+        window.setTimeout(() => {
+          getElements.messageEditor()?.focus();
+        }, 0);
+      }, 0);
+    }
+  },
+  l: () => {
+    (getElements.openSidebar() || getElements.closeSidebar())?.dispatchEvent(
+      new Event('click')
+    );
+  },
+  openSidebarVisitor: () => getElements.sidebarContent()[0].click(),
 };
