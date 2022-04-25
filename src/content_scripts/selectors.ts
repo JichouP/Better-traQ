d8444f4cc78c986a998ac6e4a7cdc6aad27e192d
JichouP <akkunn100@gmail.com>
2022-04-26 03:18:42 +0900

    fix selectors

--- src/content_scripts/selectors.ts
+++ src/content_scripts/selectors.ts
@@ -47,7 +47,7 @@ const selectors: Record<SelectorEnum, () => string | null> = {
       : null,
   navigationBarHomeTopUnreadChannel: () =>
     homeChannelExists()
-      ? '#app > div > div > div > div > div > div > div > div > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(2)' // ホームチャンネルが存在する場合は2番目
+      ? '#app > div > div > div > div > div > div > div > div > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div' // ホームチャンネルが存在する場合は2番目
       : '#app > div > div > div > div > div > div > div > div > div:nth-child(1) > div:nth-child(2) > div > div:nth-child(1)', // ホームチャンネルが存在しない場合は1番目
   navigationSelectorChannelsTab: () => navigationSelector(3),
   navigationBarChannelsChannelFilterInput: () =>
