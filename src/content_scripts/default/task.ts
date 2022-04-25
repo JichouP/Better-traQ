463fd007f25bf891c783c89d391cb5bc2ceee386
JichouP <akkunn100@gmail.com>
2022-04-26 01:39:47 +0900

    remove optional of Storage and Task

--- src/content_scripts/default/task.ts
+++ src/content_scripts/default/task.ts
@@ -1,72 +1,198 @@
 const defaultTasks: Task[] = [
   {
     name: '戻る',
-    keybinds: [{ key: 'r', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'r',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickPreviousPage'],
   },
   {
     name: '進む',
-    keybinds: [{ key: 't', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 't',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNextPage'],
   },
   {
     name: 'ホームタブをクリック',
-    keybinds: [{ key: 'q', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'q',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationSelectorHomeTab'],
   },
   {
     name: 'ホームチャンネルをクリック',
-    keybinds: [{ key: 'w', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'w',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarHomeHomeChannel'],
   },
   {
     name: 'トップ未読チャンネルをクリック',
-    keybinds: [{ key: 'e', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'e',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarHomeTopUnreadChannel'],
   },
   {
     name: 'チャンネルタブをクリック',
-    keybinds: [{ key: 'a', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'a',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationSelectorChannelsTab'],
   },
   {
     name: '現在のチャンネルまでツリーを展開',
-    keybinds: [{ key: 'A', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'A',
+        shiftKey: true,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarChannelsExpandTreeToCurrentChannel'],
   },
   {
     name: '次のチャンネルをクリック',
-    keybinds: [{ key: 'u', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'u',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarChannelsNextChannel'],
   },
   {
     name: '前のチャンネルをクリック',
-    keybinds: [{ key: 'i', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'i',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarChannelsPreviousChannel'],
   },
   {
     name: '現在のチャンネルの展開/折りたたみ',
-    keybinds: [{ key: 'o', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'o',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarChannelsChannelSelectedChannelHash'],
   },
   {
     name: 'チャンネルフィルターにフォーカス',
-    keybinds: [{ key: 's', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 's',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['focusNavigationBarChannelsChannelFilterInput'],
   },
   {
     name: 'ユーザーフィルターにフォーカス',
-    keybinds: [{ key: 'S', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'S',
+        shiftKey: true,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['focusNavigationBarUsersUserFilterInput'],
   },
   {
     name: '親チャンネル名をクリック',
-    keybinds: [{ key: 'd', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'd',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickChannelHeaderChannelNameParentChannelName'],
   },
   {
     name: 'チャンネルフィルターにフォーカスして、現在のチャンネルを入力',
-    keybinds: [{ key: 'f', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'f',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: [
       'focusNavigationBarChannelsChannelFilterInput',
       'inputCurrentChannelNameNavigationBarChannelsChannelFilter',
@@ -74,77 +200,212 @@ const defaultTasks: Task[] = [
   },
   {
     name: 'チャンネルフィルターのスターをクリック',
-    keybinds: [{ key: 'g', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'g',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarChannelsChannelFilterStar'],
   },
   {
     name: 'アクティビティタブをクリック',
-    keybinds: [{ key: 'z', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'z',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationSelectorActivityTab'],
   },
   {
     name: '「通知/未読購読チャンネルのみを表示」をクリック',
-    keybinds: [{ key: 'x', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'x',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarActivityIsNotAllToggleButton'],
   },
   {
     name: '「同じチャンネルでは一つしかメッセージを表示しない」をクリック',
-    keybinds: [{ key: 'c', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'c',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickNavigationBarActivityIsPerChannelToggleButton'],
   },
   {
     name: 'アクティビティのフォーローモードのトグル',
-    keybinds: [{ key: '-', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: '-',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['toggleNavigationBarActivityFollowMode'],
   },
   {
     name: 'メッセージ入力欄にフォーカス',
-    keybinds: [{ key: 'n', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'n',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['focusChannelViewMessageInput'],
   },
   {
     name: 'スタンプピッカーを開く',
-    keybinds: [{ key: 'm', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'm',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickChannelViewMessageInputStampButton'],
   },
   {
     name: '最新のメッセージを編集',
-    keybinds: [{ key: 'ArrowUp', conditions: ['isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'ArrowUp',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickChannelViewLatestMessageContextMenuEditMessage'],
   },
   {
     name: '次のメッセージを選択',
-    keybinds: [{ key: 'j', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'j',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['mouseoverChannelViewMessageListNextMessage'],
   },
   {
     name: '前のメッセージを選択',
-    keybinds: [{ key: 'k', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'k',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['mouseoverChannelViewMessageListPreviousMessage'],
   },
   {
     name: '一番下までスクロール',
-    keybinds: [{ key: 'b', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'b',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['scrollToBottomOfChannelView'],
   },
   {
     name: '最新のメッセージのスタンプピッカーを開く',
-    keybinds: [{ key: 'p', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'p',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickChannelViewLatestMessageStampPicker'],
   },
   {
     name: 'サイドバーのトグル',
-    keybinds: [{ key: 'l', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'l',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['toggleSidebar'],
   },
   {
     name: '選択したメッセージのスポイラーをクリック',
-    keybinds: [{ key: '@', conditions: ['!isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: '@',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['!isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['clickSpoilersOfSelectedMessage'],
   },
   {
     name: 'フォーカスを外す',
-    keybinds: [{ key: 'Escape', conditions: ['isSelectedInputOrTextarea'] }],
+    keybinds: [
+      {
+        key: 'Escape',
+        shiftKey: false,
+        ctrlKey: false,
+        altKey: false,
+        metaKey: false,
+        conditions: ['isSelectedInputOrTextarea'],
+      },
+    ],
     actions: ['blurActiveInputElement'],
   },
 ];
