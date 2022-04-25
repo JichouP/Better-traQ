effb97f2e0035bc18ee8d75fd7cf4d814a88eba8
JichouP <akkunn100@gmail.com>
2022-04-26 02:57:30 +0900

    fix migration0

--- src/content_scripts/migrations/migration0.ts
+++ src/content_scripts/migrations/migration0.ts
@@ -1,9 +1,13 @@
 import defaultTasks from '../default/task';
 import storage, { Storage } from '@/store/Storage';
+import storageSetting from '@/store/StorageSetting';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const migration0 = async (_data: Storage): Promise<Storage> => {
   await storage.clear();
+  await storageSetting.update({
+    isSettingSync: true,
+  });
   const newData: Storage = {
     migrationVersion: 0,
     task: defaultTasks,
