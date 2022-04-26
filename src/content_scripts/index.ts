1fa3bfc7aec3c88e6aa7060618fb023de116a537
JichouP <akkunn100@gmail.com>
2022-04-27 02:39:44 +0900

    remove unused comment

--- src/content_scripts/index.ts
+++ src/content_scripts/index.ts
@@ -1,12 +1,10 @@
 import migration from './migrations/migration';
 import taskHandlerBuilder from './taskHandlerBuilder';
-import storage, { unsafeGet } from '@/store/Storage';
+// import storage from '@/store/Storage';
 
 const init = async () => {
   // await storage.clear();
   await migration();
-  // const a = await unsafeGet();
-  // console.log(JSON.stringify(a).length, a);
   const taskHandler = await taskHandlerBuilder();
   document.addEventListener('keydown', taskHandler);
 };
