35cf5af6ab3d0a312d00178e1bcded38196b0c82
JichouP <akkunn100@gmail.com>
2022-04-27 11:09:58 +0900

    firefox

--- scripts/plugins/generateManifestPlugin.ts
+++ scripts/plugins/generateManifestPlugin.ts
@@ -14,38 +14,69 @@ const template = ({
   VERSION: string;
   BROWSER: string;
 }) => {
-  const manifest: Record<string, unknown> = {
-    manifest_version: 3,
-    name: `Better ${SERVICE}`,
-    version: `${VERSION}`,
-    action: {
-      default_icon: {
-        '16': '16.png',
-        '24': '24.png',
-        '32': '32.png',
-      },
-      default_title: `Better ${SERVICE}`,
-      default_popup: 'popup.html',
-    },
-    description: `An Extension that Makes ${SERVICE} Useful`,
-    icons: {
-      '16': '16.png',
-      '48': '48.png',
-      '128': '128.png',
-    },
-    permissions: ['declarativeContent', 'storage', 'scripting'],
-    background: {
-      service_worker: 'background.js',
-      type: 'module',
-    },
-    content_scripts: [
-      {
-        matches: [`https://${HOST}/*`],
-        js: ['content_scripts.js'],
-      },
-    ],
-    options_page: 'options.html',
-  };
+  const manifest: Record<string, unknown> =
+    BROWSER === 'firefox'
+      ? {
+          manifest_version: 2,
+          name: `Better ${SERVICE}`,
+          version: `${VERSION}`,
+          page_action: {
+            default_icon: {
+              '16': '16.png',
+              '24': '24.png',
+              '32': '32.png',
+            },
+            default_title: `Better ${SERVICE}`,
+            default_popup: 'popup.html',
+          },
+          description: `An Extension that Makes ${SERVICE} Useful`,
+          icons: {
+            '16': '16.png',
+            '48': '48.png',
+            '128': '128.png',
+          },
+          permissions: ['declarativeContent', 'storage', 'scripting'],
+          background: { scripts: ['background.js'], persistent: false },
+          content_scripts: [
+            {
+              matches: [`https://${HOST}/*`],
+              js: ['content_scripts.js'],
+            },
+          ],
+          options_page: 'options.html',
+        }
+      : {
+          manifest_version: 3,
+          name: `Better ${SERVICE}`,
+          version: `${VERSION}`,
+          action: {
+            default_icon: {
+              '16': '16.png',
+              '24': '24.png',
+              '32': '32.png',
+            },
+            default_title: `Better ${SERVICE}`,
+            default_popup: 'popup.html',
+          },
+          description: `An Extension that Makes ${SERVICE} Useful`,
+          icons: {
+            '16': '16.png',
+            '48': '48.png',
+            '128': '128.png',
+          },
+          permissions: ['declarativeContent', 'storage', 'scripting'],
+          background: {
+            service_worker: 'background.js',
+            type: 'module',
+          },
+          content_scripts: [
+            {
+              matches: [`https://${HOST}/*`],
+              js: ['content_scripts.js'],
+            },
+          ],
+          options_page: 'options.html',
+        };
   if (BROWSER === 'firefox') {
     manifest.browser_specific_settings = {
       gecko: {
