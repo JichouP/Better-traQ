afc7661fbd957bec5855285d7c44021b7b5e29ad
JichouP <akkunn100@gmail.com>
2022-04-27 17:42:33 +0900

    showAction

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,17 +1,29 @@
 import browser from 'webextension-polyfill';
 
-// browser.declarativeContent.onPageChanged.removeRules(undefined, () => {
-//   browser.declarativeContent.onPageChanged.addRules([
-//     {
-//       conditions: [
-//         new browser.declarativeContent.PageStateMatcher({
-//           pageUrl: { hostEquals: process.env.HOST },
-//         }),
-//       ],
-//       actions: [new browser.declarativeContent.ShowAction()],
-//     },
-//   ]);
-// });
+browser.runtime.onInstalled.addListener(() => {
+  // Page actions are disabled by default and enabled on select tabs
+  browser.action.disable();
+
+  // Clear all rules to ensure only our expected rules are set
+  browser.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    // Declare a rule to enable the action on example.com pages
+    const exampleRule = {
+      conditions: [
+        new browser.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: process.env.HOST },
+        }),
+        new browser.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: browser.runtime.id },
+        }),
+      ],
+      actions: [new browser.declarativeContent.ShowAction()],
+    };
+
+    // Finally, apply our new array of rules
+    const rules = [exampleRule];
+    browser.declarativeContent.onPageChanged.addRules(rules);
+  });
+});
 
 browser.action.onClicked.addListener(() => {
   browser.runtime.openOptionsPage();
