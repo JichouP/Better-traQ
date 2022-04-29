39d3bafe62c704f964594075d564b76f04ddcfe8
JichouP <akkunn100@gmail.com>
2022-04-29 15:54:41 +0900

    changelog

--- src/options/pages/Changelog.tsx
+++ src/options/pages/Changelog.tsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const Changelog = () => <div>Changelog</div>;
+type TemplateProps = { version: string; children: JSX.Element };
+
+const Template: FC<TemplateProps> = ({ version, children }) => (
+  <div className="mt-8 mb-8">
+    <h1 className="text-2xl">{version}</h1>
+    <ul className="mt-4 ml-8 list-disc">{children}</ul>
+  </div>
+);
+
+const Changelog = () => (
+  <div className="flex flex-col items-center justify-start overflow-y-scroll text-base">
+    <div className="w-[48rem] mt-8 mb-16 h-screen">
+      <div className="mt-8 mb-8">
+        <h1 className="text-4xl">Changelog</h1>
+      </div>
+      <Template version="v3.0.0.0">
+        <li>contents</li>
+      </Template>
+    </div>
+  </div>
+);
 
 export default Changelog;
