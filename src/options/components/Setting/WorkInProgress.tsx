427150b9af85cb2e7342c316f5bf12425e6b6627
JichouP <akkunn100@gmail.com>
2022-04-29 12:28:36 +0900

    WIP

---
+++ src/options/components/Setting/WorkInProgress.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const WorkInProgress = () => (
+  <div className="ml-auto mr-auto shadow-lg alert alert-info w-fit">
+    <div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="flex-shrink-0 w-6 h-6 stroke-current"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span className="text-base">
+        キーバインド設定は仮実装です。手伝ってくれる方は{' '}
+        <code>#gps/times/JichouP</code> まで
+      </span>
+    </div>
+  </div>
+);
+
+export default WorkInProgress;
--- src/options/pages/Setting.tsx
+++ src/options/pages/Setting.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
 import Task from '@/options/components/Setting/Task';
 import ThemeToggleButton from '@/options/components/Setting/ThemeToggleButton';
+import WorkInProgress from '@/options/components/Setting/WorkInProgress';
 
 const Setting = () => (
   <div className="flex flex-col items-center overflow-y-scroll">
     <div className="w-[48rem] mt-8 mb-16">
-      <div className="ml-auto mr-auto shadow-lg alert alert-info w-fit">
-        <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="flex-shrink-0 w-6 h-6 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span className="text-base">
-            キーバインド設定は仮実装です。手伝ってくれる方は{' '}
-            <code>#gps/times/JichouP</code> まで
-          </span>
-        </div>
-      </div>
+      <WorkInProgress />
       <div className="mt-8">
         <h1 className="text-2xl">Better traQ の設定</h1>
         <div className="flex flex-row items-center mt-4 mb-4 ml-8">
