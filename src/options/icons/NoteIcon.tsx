a1cf54e01b1be7bc364af5996d759505a57ace68
JichouP <akkunn100@gmail.com>
2022-04-29 11:39:59 +0900

    add NoteIcon

---
+++ src/options/icons/NoteIcon.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+
+type Props = {
+  className: string;
+};
+
+const NoteIcon: FC<Props> = ({ className }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    strokeWidth="2"
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" />
+    <rect x="5" y="3" width="14" height="18" rx="2" />
+    <line x1="9" y1="7" x2="15" y2="7" />
+    <line x1="9" y1="11" x2="15" y2="11" />
+    <line x1="9" y1="15" x2="13" y2="15" />
+  </svg>
+);
+
+export default NoteIcon;
