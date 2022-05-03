679416e468eb2d7330be7839d0eab07a7cd56306
JichouP <akkunn100@gmail.com>
2022-05-04 02:50:34 +0900

    チャンネル/ユーザー検索にフォーカスしているかどうかを判定する条件を追加 Fixes #55

---
+++ src/content_scripts/conditions/notIsFocusNavigationBarChannelsChannelFilterInput.ts
@@ -0,0 +1,6 @@
+import isFocusNavigationBarChannelsChannelFilterInput from './isFocusNavigationBarChannelsChannelFilterInput';
+
+const notIsFocusNavigationBarChannelsChannelFilterInput = (): boolean =>
+  !isFocusNavigationBarChannelsChannelFilterInput();
+
+export default notIsFocusNavigationBarChannelsChannelFilterInput;
---
+++ src/content_scripts/conditions/notIsFocusNavigationBarUsersUserFilterInput.ts
@@ -0,0 +1,6 @@
+import isFocusNavigationBarUsersUserFilterInput from './isFocusNavigationBarUsersUserFilterInput';
+
+const notIsFocusNavigationBarUsersUserFilterInput = (): boolean =>
+  !isFocusNavigationBarUsersUserFilterInput();
+
+export default notIsFocusNavigationBarUsersUserFilterInput;
