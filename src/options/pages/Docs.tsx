3aa80229d676f386c271158549f804f4d36f4e32
JichouP <akkunn100@gmail.com>
2022-05-06 06:11:59 +0900

    ドキュメントを修正 Fixes #42

--- src/options/pages/Docs.tsx
+++ src/options/pages/Docs.tsx
@@ -54,21 +54,22 @@ const Docs = () => {
         <div className="mt-8 mb-8">
           <h2 className="text-2xl font-bold">条件 (condition)</h2>
           <p className="mt-2">
-            キーを押してBetter
-            traQがタスクを実行するときに満たしているべき状態のことを{' '}
+            キーを押して Better traQ
+            がタスクを実行するときに満たしているべき状態のことを{' '}
             <span className="font-bold">条件 (condition)</span>{' '}
-            と呼びます。条件は複数指定することができます。これらの条件のうち
-            <span className="font-bold">どれかひとつでも</span>
-            満たしていない場合は Better traQ はタスクを実行しません。
+            と呼びます。条件は複数指定することができます。これらの条件を
+            <span className="font-bold">すべて満たしている場合のみ</span> Better
+            traQ はタスクを実行します。
           </p>
           <p>
-            例として、メッセージの入力を考えます。このとき Better traQ
+            例として、新規メッセージの入力を考えます。このとき Better traQ
             が反応してしまうと、メッセージの入力中にもかかわらず、別のチャンネルを開くタスクを実行したりしてしまいます。こうなると正常に
             traQ
             を使用できなくなってしまうため、デフォルトのほとんどのタスクには{' '}
-            <code>notIsFocusedInputOrTextarea</code>{' '}
-            という条件がついています。これによって、メッセージの入力中にBetter
-            traQが反応することを防いでいます。
+            <code>niFInputOrTextarea</code> (
+            <code>notIsFocusedInputOrTextarea</code> の略){' '}
+            という条件がついています。これによって、メッセージの入力中に Better
+            traQ が反応することを防いでいます。
           </p>
           <p>
             使用可能な条件の一覧については{' '}
@@ -89,6 +90,18 @@ const Docs = () => {
             キーを押したときに実行する処理（関数）のことを{' '}
             <span className="font-bold">アクション (action)</span>{' '}
             と呼びます。ひとつのタスクに対して、複数のアクションを指定する事ができます。タスクはこれらのアクションを先頭からひとつずつ順番に実行します。もし、同時に複数のアクションを起こしたい場合は、複数のタスクを同じキーに登録してください。ただし、想定外の挙動をする可能性があるためご注意ください。通常は、ひとつのタスクに複数のアクションを設定すれば十分です。
+          </p>
+          <p>
+            使用可能なアクションの一覧については{' '}
+            <a
+              href="https://github.com/JichouP/Better-traQ/blob/main/src/store/zAction.ts"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link link-primary"
+            >
+              こちら
+            </a>{' '}
+            をご覧ください。
           </p>
         </div>
         <div className="mt-8 mb-8">
@@ -121,7 +134,28 @@ const Docs = () => {
             >
               設定
             </a>{' '}
-            からタスクの設定をデフォルトに戻してみてください。それでも動かない場合は、お手数ですが{' '}
+            からタスクの設定をデフォルトに戻してみてください。それでも動かない場合は、{' '}
+            <a
+              className="link link-primary"
+              onClick={() => {
+                setPage('setting');
+              }}
+              onKeyDown={onKeyDownHandler}
+            >
+              設定
+            </a>{' '}
+            からストレージを初期化してみてください。もし、それでも動かない場合はお手数ですが{' '}
+            <code>
+              <a
+                className="link link-primary"
+                href={`https://${process.env.HOST}/channels/gps/times/JichouP`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                #gps/times/JichouP
+              </a>
+            </code>{' '}
+            までご連絡いただくか、
             <a
               href="https://github.com/JichouP/Better-traQ/issues/new"
               target="_blank"
